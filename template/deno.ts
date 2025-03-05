38408915334e4456d29fe8efe4061778a660a7a5
kpawlik <kamil.pawlik@capgemini.com>
2025-03-05 11:15:02 +0100

    Added fast task template

---
+++ template/deno.ts
@@ -0,0 +1,13 @@
+import { assertEquals } from "jsr:@std/assert";
+
+/*   *   *   *   *   *   *   *   *   *   */
+/*   *   *   *   *   *   *   *   *   *   */
+
+/*   *   *   *   *   *   *   *   *   *   */
+/*   *   *   *   *   *   *   *   *   *   */
+
+Deno.test("Case 1", () => {
+});
+
+Deno.test("Case 2", () => {
+});
