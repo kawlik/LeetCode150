dd0a72c01edb73035a443b80cf7a455b30e824bb
kpawlik <kamil.pawlik@capgemini.com>
2025-03-24 14:59:53 +0100

    solved #141 - Linked List Cycle

---
+++ 141/main.ts
@@ -0,0 +1,147 @@
+import { assertEquals } from "jsr:@std/assert";
+
+/*   *   *   *   *   *   *   *   *   *   */
+/*   *   *   *   *   *   *   *   *   *   */
+
+class ListNode {
+	val: number;
+	next: ListNode | null;
+	constructor(val?: number, next?: ListNode | null) {
+		this.val = val === undefined ? 0 : val;
+		this.next = next === undefined ? null : next;
+	}
+}
+
+/*   *   *   *   *   *   *   *   *   *   */
+/*   *   *   *   *   *   *   *   *   *   */
+
+function hasCycle(head: ListNode | null): boolean {
+	const visitedNodes = new Set<ListNode>();
+
+	let node: ListNode;
+
+	if (head) {
+		node = head;
+		visitedNodes.add(node);
+
+		while (node.next) {
+			node = node.next;
+
+			if (visitedNodes.has(node)) return true;
+			else visitedNodes.add(node);
+		}
+	}
+
+	return false;
+}
+
+/*   *   *   *   *   *   *   *   *   *   */
+/*   *   *   *   *   *   *   *   *   *   */
+
+Deno.test("Case 1", () => {
+	const head = [3, 2, 0, -4];
+	const pos = 1;
+
+	const root = new ListNode(head[0]);
+	let node: ListNode;
+	let last: ListNode;
+
+	node = root;
+
+	for (let i = 1; i < head.length; i++) {
+		node.next = new ListNode(head[i]);
+		node = node.next!;
+	}
+
+	last = node;
+	node = root;
+
+	for (let i = 0; i < pos; i++) {
+		node = node.next!;
+	}
+
+	last.next = node;
+
+	assertEquals(hasCycle(root), true);
+});
+
+Deno.test("Case 2", () => {
+	const head = [1, 2];
+	const pos = 0;
+
+	const root = new ListNode(head[0]);
+	let node: ListNode;
+	let last: ListNode;
+
+	node = root;
+
+	for (let i = 1; i < head.length; i++) {
+		node.next = new ListNode(head[i]);
+		node = node.next!;
+	}
+
+	last = node;
+	node = root;
+
+	for (let i = 0; i < pos; i++) {
+		node = node.next!;
+	}
+
+	last.next = node;
+
+	assertEquals(hasCycle(root), true);
+});
+
+Deno.test("Case 3", () => {
+	const head = [1];
+	const pos = -1;
+
+	const root = new ListNode(head[0]);
+	let node: ListNode;
+	let last: ListNode;
+
+	node = root;
+
+	for (let i = 1; i < head.length; i++) {
+		node.next = new ListNode(head[i]);
+		node = node.next!;
+	}
+
+	last = node;
+	node = root;
+
+	for (let i = 0; i < pos; i++) {
+		node = node.next!;
+	}
+
+	if (pos >= 0) last.next = node;
+
+	assertEquals(hasCycle(root), false);
+});
+
+Deno.test("Case 4", () => {
+	const head = [1, 2];
+	const pos = -1;
+
+	const root = new ListNode(head[0]);
+	let node: ListNode;
+	let last: ListNode;
+
+	node = root;
+
+	for (let i = 1; i < head.length; i++) {
+		node.next = new ListNode(head[i]);
+		node = node.next!;
+	}
+
+	last = node;
+	node = root;
+
+	for (let i = 0; i < pos; i++) {
+		node = node.next!;
+	}
+
+	if (pos >= 0) last.next = node;
+
+	assertEquals(hasCycle(root), false);
+});
